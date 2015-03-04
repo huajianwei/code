66d455752ca9a91b8ffbf92365208170833f59f3
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-04 17:19:18 +0800

    sent by work pc

---
+++ learn/the_note_of_dajiawang/string_function.py
@@ -0,0 +1,26 @@
+__author__ = 'huajw'
+#coding=utf-8
+#所有的序列标准操作对字符串都适用，但是不能赋值。
+a = 'www.baidu.com'
+print a[0:3]
+print a[-3:]
+#a[-3:]  = "org"
+print a
+# 格式化输出
+
+print "Hello,%s,do you %s enough?" %("big","clever")
+
+
+
+
+
+
+
+
+
+
+
+print '\n'*3
+print  "%s"  % __file__
+
+
