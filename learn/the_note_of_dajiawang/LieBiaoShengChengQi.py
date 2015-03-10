31389113caaa02400dbf8c19ab758e2aeeb37e28
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-10 23:20:49 +0800

    sent by my laptop

---
+++ learn/the_note_of_dajiawang/LieBiaoShengChengQi.py
@@ -0,0 +1,30 @@
+__author__ = 'Administrator'
+#coding=utf-8
+
+a = range(10)
+print a
+
+
+l=[]
+for x in range(1,11):
+	l.append(x*x)
+
+print l
+
+#使用列表生成式
+
+l = [x*x for x in range(1,11)]
+l = [x*x for x in range(1,11) if x % 2 == 0]
+print l
+ll = [m+n for m in 'abcd' for n in '1234']
+print ll
+
+
+# 列出当前目录下的所有文件和目录名
+import os
+
+print [d for d in os.listdir('.')]
+
+# for循环其实可以同时使用两个甚至多个变量
+
+
