57b0a19494484833a956bca69f3ed7902646e506
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-13 14:49:40 +0800

    sent by work pc

---
+++ learn/mokuai_test/DaiRuMoKuai.py
@@ -0,0 +1,9 @@
+__author__ = 'huajw'
+#coding=utf-8
+import sys
+sys.path.append("G:\my\code\learn\the_note_of_dajiawang")
+#print __file__
+#print sys.path
+import ShiYongMoKuai
+
+ShiYongMoKuai.test()
