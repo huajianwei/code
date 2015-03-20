522ffc6db44b5c7538d5a46c92edb3926531cece
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-20 14:01:07 +0800

    sent by work pc

---
+++ learn/MianXiangDuiXiang/LeiDeShiXian.py
@@ -0,0 +1,18 @@
+__author__ = 'huajw'
+#coding = utf-8
+
+# a new class  student
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print '%s: %s' % (self.name, self.score)
+
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+bart.print_score()
+lisa.print_score()
